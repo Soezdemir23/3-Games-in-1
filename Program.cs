794ffda5c08ad0d92ba37ec8e1208e64f524dec7
Serdar Özdemir <90345250+Soezdemir23@users.noreply.github.com>
2023-08-02 06:19:35 +0200

    Borked Github Profile, manual upload
    Updated GameLogic in Rock-Paper-Scissors to use Keys instead of strings for choices.
    Updated Logic Values and Menu Logic to make it more coherent for both players and devs.
    Also added a preliminary Mainmenu

--- Program.cs
+++ Program.cs
@@ -4,6 +4,38 @@
 
 
 using _3_Games_in_1.Rock_Paper_Scissors;
+new GameLogicRPS();
 
-GameLogicRPS rps = new GameLogicRPS();
+//var exitProgram = false;
+//while (!exitProgram)
+//{
+//	Console.WriteLine("\t\t\t\t\t\t3-in-1 Game Collection!");
+//	Console.WriteLine("\t\t\t\t\t\tChoose your options!");
+//	Console.WriteLine("\t\t\t\t\t\t1. Rock Paper Scissors");
+//	Console.WriteLine("\t\t\t\t\t\t2. 15-Puzzle");
+//	Console.WriteLine("\t\t\t\t\t\t3. Hangman");
+//	Console.WriteLine("\t\t\t\t\t\t4. Exit");
 
+//	var input = Console.ReadKey();
+
+//	switch (input.Key)
+//	{
+//		case ConsoleKey.D1:
+//			new GameLogicRPS();
+//			break;
+//		case ConsoleKey.D2:
+//			Console.WriteLine("15-Puzzle");
+//			break;
+//		case ConsoleKey.D3:
+//			Console.WriteLine("Hangman");
+//			break;
+//		case ConsoleKey.D4:
+//			Console.WriteLine("Exiting...");
+//			exitProgram = true;
+//			break;
+//		default:
+//			Console.WriteLine("Invalid input!");
+//			break;
+//	}
+//}
+//Console.WriteLine("\t\t\t\t\t\t\tGoodbye!");
